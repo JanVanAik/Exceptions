c009377913865d31a8e2149ffeb5afadd10f324b
JanVanAik <ar.orlovskiy@gmail.com>
2023-07-25 22:20:18 +0300

    task 4 done

--- src/main/java/Lesson_2/task_4.java
+++ src/main/java/Lesson_2/task_4.java
@@ -1,4 +1,14 @@
 package Lesson_2;
 
+import java.util.Scanner;
+
 public class task_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String UserInput = scanner.nextLine();
+        if (UserInput == ""){
+            throw new RuntimeException("Do not input empty line");
+        }
+
+    }
 }
