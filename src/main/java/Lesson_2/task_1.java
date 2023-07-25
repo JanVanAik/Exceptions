726d2f7f60c3187353300c8be66dabace7e58c33
JanVanAik <ar.orlovskiy@gmail.com>
2023-07-25 22:10:16 +0300

    task 1 done

--- src/main/java/Lesson_2/task_1.java
+++ src/main/java/Lesson_2/task_1.java
@@ -1,4 +1,19 @@
 package Lesson_2;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class task_1 {
+    public static void main(String[] args) {
+        System.out.println("Input float number: ");
+        while (true) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                float num = scanner.nextFloat();
+                System.out.println(num);
+                break;
+            } catch (InputMismatchException e){
+                System.out.println("Not a float. Input float number: ");
+            }
+        }
+    }
 }
